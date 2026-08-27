f8d92d6429c97f7e56faa68baf8508a92b74046b
Joodar <joodartynystanov@gmail.com>
2026-08-27 20:57:29 +0600

    миграции для роли

---
+++ src/main/java/org/edufood/edufood/entities/Role.java
@@ -0,0 +1,33 @@
+package org.edufood.edufood.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /** Имя роли в формате Spring Security, например ROLE_USER. */
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @Builder.Default
+    private Set<User> users = new HashSet<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
